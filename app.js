0a6316fab71065369696114d64c654ff41421d19
Shital Patil <patilshitalbharat@gmail.com>
2025-09-03 01:20:55 +0530

    added crud routes

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app= express();
 const mongoose= require('mongoose');
 const Listing= require("./models/listing");
 const path= require("path");
+const methodOverride= require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true})); //to parse all data that from request 
+app.use(methodOverride("_method"));
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
@@ -25,12 +27,43 @@ app.get('/listings',async(req,res)=>{
 
 });
 
+//New Route
+app.get('/listings/new', (req,res)=>{
+    res.render("listings/new.ejs");
+});
+
 //Show Route
 app.get('/listings/:id', async(req,res)=>{
     let {id}= req.params;
     const listing= await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
 });
+
+//Create Route
+app.post('/listings', async(req,res)=>{
+    console.log(req.body);
+    // let {title, description ,image, price, country, location }= req.body;
+    // let data= new Listing({title : title, description: description, image}); // long syntax for creating Listing model instance for inserting data in collection
+    const newListing= new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect('/listings');
+});
+
+//Edit Route
+app.get('/listings/:id/edit', async(req,res)=>{
+    let {id}= req.params;
+    const listing= await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+//Update Route
+app.put('/listings/:id', async(req,res)=>{
+    let {id}= req.params;
+    console.log(req.body.listing);
+    await Listing.findByIdAndUpdate(id, {...req.body.listing}, {new: true , runValidators: true});// spread operator
+    res.redirect(`/listings/${id}`);
+});
+
 
 app.listen(8080, ()=>{
     console.log("Server is listening to port 8080");
