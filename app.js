987e84a86b2a977aa7e7b18dbb991b4ecf084477
Shital Patil <patilshitalbharat@gmail.com>
2025-09-02 23:17:08 +0530

    added show route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path= require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true})); //to parse all data that from request 
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
@@ -22,6 +23,13 @@ app.get('/listings',async(req,res)=>{
     let listings= await Listing.find();
     res.render("listings/index.ejs",{listings});
 
+});
+
+//Show Route
+app.get('/listings/:id', async(req,res)=>{
+    let {id}= req.params;
+    const listing= await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
 });
 
 app.listen(8080, ()=>{
