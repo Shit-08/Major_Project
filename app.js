54ea0fcb3d157a7dc429332c52594eecaeb04477
Shital Patil <patilshitalbharat@gmail.com>
2025-09-11 17:14:19 +0530

    added custom error.ejs

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.use((req,res,next)=>{
 //error handling middleware
 app.use((err,req,res,next)=>{
     let {statusCode=500, message="Something went wrong"} = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", {err});
 })
 
 app.listen(8080, ()=>{
