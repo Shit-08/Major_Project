4e5faed396dc3b46a1899886fe9fcbdba08b48e6
Shital Patil <patilshitalbharat@gmail.com>
2025-10-04 23:25:52 +0530

    set marker and popup

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,19 @@
 
 mapboxgl.accessToken = mapToken;
+
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    style: 'mapbox://styles/mapbox/standard', // style URL
+    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
 
 const marker = new mapboxgl.Marker({color: 'red'})
-    .setLngLat(coordinates) //Listing.geometry.coor
+    .setLngLat(listing.geometry.coordinates) //Listing.geometry.coordinates
+    .setPopup(
+        new mapboxgl.Popup({offset: 25}).setHTML(
+            `<h4> ${listing.location} </h4>
+            <p> Exact location will be provided after booking </p>`
+        )
+        ) //add popup
     .addTo(map);
