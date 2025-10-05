868645da37ad667230106b2dcbfee4e84343eae9
Shital Patil <patilshitalbharat@gmail.com>
2025-10-05 13:08:15 +0530

    added search option in navbar

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 //to use dotenv when not in production phase/env
-if(process.env.NODE_ENV != "production"){
-    require('dotenv').config();
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
-const express= require("express");
-const app= express();
-const mongoose= require('mongoose');
-const path= require("path");
-const methodOverride= require("method-override");
-const ejsMate= require('ejs-mate');
-const ExpressError = require('./utils/expressError');
-const listingRouter= require('./routes/listing');
-const reviewRouter = require('./routes/review');
-const userRouter = require('./routes/user');
-const session = require('express-session');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/expressError");
+const listingRouter = require("./routes/listing");
+const reviewRouter = require("./routes/review");
+const userRouter = require("./routes/user");
+const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -20,21 +20,21 @@ const User = require("./models/user");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true})); //to parse all data that from request 
+app.use(express.urlencoded({ extended: true })); //to parse all data that from request
 app.use(methodOverride("_method")); //middleware for method override
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
-const sessionOptions= {
-    secret: "mysupersecretcode",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true
-    }
-}
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
 app.use(session(sessionOptions));
 app.use(flash());
 app.use(passport.initialize());
@@ -45,39 +45,39 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 
 main()
-.then(()=>{
+  .then(() => {
     console.log("connected to DB");
-})
-.catch(err=>console.log(err));
+  })
+  .catch((err) => console.log(err));
 
-app.use((req,res, next)=>{
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
-})
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
 
-app.use('/listings', listingRouter);
-app.use('/listings/:id/reviews', reviewRouter);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
 //it will run for every request that didn’t match earlier routes.
-app.use((req,res,next)=>{
-    next(new ExpressError(404,"Page not found"));
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page not found"));
 });
 
 //error handling middleware
-app.use((err,req,res,next)=>{
-    let {statusCode=500, message="Something went wrong"} = err;
-    res.status(statusCode).render("error.ejs", {err});
-})
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", { err });
+});
 
-app.listen(8080, ()=>{
-    console.log("Server is listening to port 8080");
+app.listen(8080, () => {
+  console.log("Server is listening to port 8080");
 });
 
 // app.get('/', (req, res)=>{
--- controllers/listings.js
+++ controllers/listings.js
@@ -1,77 +1,84 @@
 const Listing = require("../models/listing");
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapToken = process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
-module.exports.index=async(req,res)=>{
-    let listings= await Listing.find();
-    res.render("listings/index.ejs",{listings});
-}
-
-module.exports.new = (req,res)=>{
-    res.render("listings/new.ejs");
-}
-
-module.exports.show = async(req,res)=>{
-    let {id}= req.params;
-    const listing= await Listing.findById(id).populate({path: "reviews", populate: {path: "author"}}).populate("owner");
-    if(!listing){
-        req.flash("error", "Listing you are trying to access does not exist");
-        return res.redirect('/listings');
-    }
-    res.render("listings/show.ejs", {listing});
+module.exports.index = async (req, res) => {
+  let listings = await Listing.find();
+  res.render("listings/index.ejs", { listings });
 };
 
-module.exports.create = async(req,res,next)=>{
-    let response = await geocodingClient.forwardGeocode({
-        query: req.body.listing.location,
-        limit: 1
+module.exports.new = (req, res) => {
+  res.render("listings/new.ejs");
+};
+
+module.exports.show = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("owner");
+  if (!listing) {
+    req.flash("error", "Listing you are trying to access does not exist");
+    return res.redirect("/listings");
+  }
+  res.render("listings/show.ejs", { listing });
+};
+
+module.exports.create = async (req, res, next) => {
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
     })
     .send();
 
-    // let {title, description ,image, price, country, location }= req.body;
-    // let data= new Listing({title : title, description: description, image}); // long syntax for creating Listing model instance for inserting data in collection
-    
+  // let {title, description ,image, price, country, location }= req.body;
+  // let data= new Listing({title : title, description: description, image}); // long syntax for creating Listing model instance for inserting data in collection
+
+  let url = req.file.path;
+  let filename = req.file.filename;
+  const newListing = new Listing(req.body.listing);
+  newListing.owner = req.user._id;
+  newListing.image = { url, filename };
+  newListing.geometry = response.body.features[0].geometry;
+  await newListing.save();
+  console.log(newListing);
+  req.flash("success", "New Listing created!");
+  res.redirect("/listings");
+};
+
+module.exports.edit = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
+    req.flash("error", "Listing you are trying to access does not exist");
+    return res.redirect("/listings");
+  }
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+  res.render("listings/edit.ejs", { listing, originalImageUrl });
+};
+
+module.exports.update = async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findByIdAndUpdate(
+    id,
+    { ...req.body.listing },
+    { new: true, runValidators: true }
+  ); // spread operator
+  if (req.file) {
     let url = req.file.path;
     let filename = req.file.filename;
-    const newListing = new Listing(req.body.listing);
-    newListing.owner = req.user._id;
-    newListing.image = {url, filename};
-    newListing.geometry = response.body.features[0].geometry;
-    await newListing.save();
-    console.log(newListing);
-    req.flash("success", "New Listing created!");
-    res.redirect('/listings');
+    listing.image = { url, filename };
+    await listing.save();
+  }
+  req.flash("success", "Listing Updated");
+  res.redirect(`/listings/${id}`);
 };
 
-module.exports.edit = async(req,res)=>{
-    let {id}= req.params;
-    const listing= await Listing.findById(id);
-    if(!listing){
-        req.flash("error", "Listing you are trying to access does not exist");
-        return res.redirect('/listings');
-    }
-    let originalImageUrl = listing.image.url;
-    originalImageUrl=originalImageUrl.replace("/upload", "/upload/w_250");
-    res.render("listings/edit.ejs",{listing, originalImageUrl});
-};
-
-module.exports.update = async(req,res)=>{
-    let {id}= req.params;
-    let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing}, {new: true , runValidators: true});// spread operator
-    if(req.file){
-        let url = req.file.path;
-        let filename = req.file.filename;
-        listing.image = {url, filename};
-        await listing.save();
-    }
-    req.flash("success", "Listing Updated");
-    res.redirect(`/listings/${id}`);
-};
-
-module.exports.delete = async(req,res)=>{
-    let {id}= req.params;
-    let deletedListing= await Listing.findByIdAndDelete(id);
-    req.flash("success", "Listing Deleted!");
-    res.redirect('/listings');
+module.exports.delete = async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  req.flash("success", "Listing Deleted!");
+  res.redirect("/listings");
 };
